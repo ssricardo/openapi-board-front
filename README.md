4dec85a34e4b24d19032f5c8819fa9b48eda7d3e
Ricardo SS <ricardo.rss.dev@gmail.com>
2019-10-09 00:16:18 +0200

    Initial commit. Most of base components created.

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # openapi-board-front
+
 Angular Front End for the OpenAPI Board App
---
+++ src/app/namespaces/namespace-info/namespace-info.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { NamespaceInfoComponent } from './namespace-info.component';
+
+describe('NamespaceInfoComponent', () => {
+  let component: NamespaceInfoComponent;
+  let fixture: ComponentFixture<NamespaceInfoComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ NamespaceInfoComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(NamespaceInfoComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
