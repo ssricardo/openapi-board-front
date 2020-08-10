1a9d335b49e8bc3efb5786a026f273fc97815a9e
ricardoss <rss.ricardo@gmail.com>
2020-08-10 20:28:30 +0200

    Small fixed on texts and load indicator

--- src/app/views/login/login.component.ts
+++ src/app/views/login/login.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import {AuthenticationService} from "../../services/authentication.service";
 import {Router} from "@angular/router";
 import {LoginData} from "../../models/models";
+import { NotificationService } from 'src/app/services/notification.service';
 
 @Component({
   selector: 'app-login',
@@ -20,7 +21,8 @@ export class LoginComponent implements OnInit {
   roles: string[] = [];
 
   constructor(private authService: AuthenticationService,
-              private router: Router) { }
+              private router: Router,
+              private notificationService: NotificationService) { }
 
   ngOnInit() {
     if (this.authService.getUser()) {
@@ -38,7 +40,12 @@ export class LoginComponent implements OnInit {
           this.router.navigate(['/']);
         },
         err => {
-            this.errorMessage = err.error;
+            if (err.status == 401 || err.status == 403) {
+              this.errorMessage = 'Reason: ' + err.error + ' Please use valid credentials.';
+            } else {
+              this.notificationService.showError(`A non-identified error happened. If it persists, inform an adminstrator.
+                For details, check the console.`);
+            }
         }
     );
   }
