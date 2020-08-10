82a49626233f19393e62f2fc1fd4aa158a64519f
Ricardo SS <ricardo.rss.dev@gmail.com>
2020-08-11 00:39:47 +0200

    Revert "Changes for Angular 9"
    This reverts commit 69f7e7c77a7321c7e08851bf361aea0a09e41927.

--- src/app/views/subscription/subscriber-list/subscriber-list.component.ts
+++ src/app/views/subscription/subscriber-list/subscriber-list.component.ts
@@ -4,7 +4,7 @@ import {SubscriberService} from "../../../services/subscriber.service";
 import {NotificationService} from "../../../services/notification.service";
 import {Router} from "@angular/router";
 import { ConfirmDialogComponent, ConfirmDialogModel } from 'src/app/confirm-dialog/ConfirmDialogComponent';
-import { MatDialog } from '@angular/material/dialog';
+import { MatDialog } from '@angular/material';
 
 @Component({
   selector: 'app-subscriber-list',
